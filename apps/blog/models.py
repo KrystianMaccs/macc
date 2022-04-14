8b97215f13b02c0c061182ab560469081b678308
Krystian Maccs <krystianmaccs@gmail.com>
2022-04-14 20:49:58 +0100

    feat: blog models

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from taggit.managers import TaggableManager
+from apps.common.models import TimeStampedUUIDModel
 
-# Create your models here.
+User = get_user_model()
+
+class Post(TimeStampedUUIDModel):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    #tags = TaggableManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('apps/blogs:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
+class Comment(TimeStampedUUIDModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created_at',)
+
+    def __str__(self):
+        return f"comment by {self.name} on {self.post}"
