7045d73ed54b5c3f8b68f99369aed656a94f1595
Krystian Maccs <krystianmaccs@gmail.com>
2022-04-24 17:19:14 +0100

    feat: added navbar component

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from .views import *
 
 urlpatterns = [
     path("krystian", PersonalInfoListView.as_view()),
-    path("create", PersonalInfoCreateView.as_view()),
     path("<int:pk>", PersonalInfoEditView.as_view()),
+    path("create", PersonalInfoCreateView.as_view()),
     path("<int:pk>/delete", PersonalInfoDeleteView.as_view()),
     path("work", WorkListView.as_view()),
     path("work-create", WorkCreateView.as_view()),
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { Component } from 'react';
+
+class HelloWorldClassComponent extends Component {
+  render() {
+    return (
+    <h1>Hello, World!</h1>
+    )
+  }
+}
