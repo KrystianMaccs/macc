e1e56a034939ce6d1f736216e69d17120740a559
Krystian Maccs <krystianmaccs@gmail.com>
2022-05-21 15:53:25 +0100

    feat: refactored frontend files

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -1,12 +1,15 @@
-
+import { useState } from 'react';
 const Contact = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
     return ( 
         <section className="contact container" id="contact">
             <h2 className="heading">Contact</h2>
 
             <form action="" className="contact-form" id="contact-form">
-                <input type="text" placeholder="Your Name" className="name" required/>
-                <input type="email" name="" id="" placeholder="Your Email" className="email" required/>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)}placeholder="Your Name" className="name" required/>
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}name="" id="" placeholder="Your Email" className="email" required/>
                 <textarea name="" id="" cols="30" rows="10" placeholder="Write message here..." className="message"></textarea>
                 <input type="submit" value="send" className="send-btn" />
             </form>
